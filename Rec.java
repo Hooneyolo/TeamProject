f6133c36eac71db1eeb7db27304d66bbec6dcbc7
정찬호 <elvpfhhrm@gmail,com>
2019-09-03 14:56:52 +0900

    REc클래스 완성했습니다.

---
+++ Rec.java
@@ -0,0 +1,22 @@
+
+/**
+ * Rec 클래스는 직사각형의 형태와 넓이를 알려주는 클래스입니다.
+ * 
+ * @author (2019243087 정찬호) 
+ * @version (20190903)
+ */
+public class Rec implements Shape
+{
+    int width;
+    int hight;
+    public Rec(int width, int hight){
+        this.width=width;
+        this.hight=hight;
+    }
+    public void draw(){
+        System.out.println(width+"x"+hight+"크기의 사각형입니다.");
+    }
+    public double getArea(){
+        return width*hight;
+    }
+}
