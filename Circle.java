0cefbe0f4685999323093ae543dc16f64fa2de8a
Seiya <seiya.u77@gmail.com>
2019-09-04 14:56:12 +0900

    변수의 이름에 this를 붙였습니다.

--- Circle.java
+++ Circle.java
@@ -13,7 +13,7 @@ public class Circle implements Shape
     }
 
     public void draw(){
-        System.out.println("반지름이 "+radius+"인 원입니다.");
+        System.out.println("반지름이 "+this.radius+"인 원입니다.");
     }
 
     public double getArea(){
