512c5b8142f8c22c6282d1e738f34d1a2a300653
Yoo Jehun <dbwpgns0525@gmail.com>
2019-09-04 11:07:32 +0900

    Circle 클래스입니다.

---
+++ Circle.java
@@ -0,0 +1,23 @@
+
+/**
+ * Cicrel의 PI,반지름을 통해 형태와 넓이를 알려주는 클래스입니다.
+ * 
+ * @author (2018315005 유제훈) 
+ * @version (2019/09/04)
+ */
+public class Circle implements Shape
+{
+    final double PI = 3.14;
+    double radius;
+    public Circle(double radius){
+        this.radius=radius;
+    }
+
+    public void draw(){
+        System.out.println("반지름이"+radius+"인 원입니다.");
+    }
+
+    public double getArea(){
+        return PI*radius;
+    }
+}
