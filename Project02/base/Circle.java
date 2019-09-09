c946d7681e0882bd4568f68ba7864e4f6fd2471e
Seiya <seiya.u77@gmail.com>
2019-09-09 13:42:25 +0900

    Circle 클래스 생성, equals() 메소드 작성

---
+++ Project02/base/Circle.java
@@ -0,0 +1,31 @@
+package Project02.base;
+import Project02.myInterface.Shape;
+/**
+ * 좌표와 반지름의 길이를 입력받아 좌표의 위치를 비교하는 클래스
+ *
+ * @author (2018315056 우메모토 세이야)
+ * @version (2019/09/09)
+ */
+public class Circle implements Shape
+{
+    private int x, y, radius;
+    public Circle(int x, int y, int radius){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public void draw(){
+        System.out.println("반지름이 "+this.radius+"인 원입니다.");
+    }
+
+    public double getArea(){
+        return this.PI * this.radius * this.radius;
+    }
+    
+    public boolean equals(Object obj) {
+        Circle c = (Circle)obj;
+        if(this.x == c.x && this.y == c.y) return true;
+        else return false;
+    }
+}
