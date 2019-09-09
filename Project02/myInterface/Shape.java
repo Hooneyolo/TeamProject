07b0c8572996d69aec2a6339c4d560734dcf963a
정찬호 <elvpfhhrm@gmail,com>
2019-09-09 13:40:42 +0900

    인터페이스 수정

---
+++ Project02/myInterface/Shape.java
@@ -0,0 +1,20 @@
+package Project02.myInterface;
+
+
+/**
+ *   인터페이스 Shape - Circle 클래스가 상속받을 인터페이스입니다.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전 번호나 날짜)
+ */
+
+public interface Shape
+{
+    /**
+     *  예제 메소드 해더 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 
+     * @param  y    메소드의 예제 매개변수
+     * @return    예제메소드의 결과 산출물
+     */
+    int sampleMethod(int y);
+}
