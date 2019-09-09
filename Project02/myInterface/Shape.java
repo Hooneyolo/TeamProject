3a68bb99144fcd8e3bb2114e30d317abe662b038
정찬호 <elvpfhhrm@gmail,com>
2019-09-09 13:42:30 +0900

    인터페이스 재수정

--- Project02/myInterface/Shape.java
+++ Project02/myInterface/Shape.java
@@ -4,17 +4,17 @@ package Project02.myInterface;
 /**
  *   인터페이스 Shape - Circle 클래스가 상속받을 인터페이스입니다.
  * 
- * @author (작성자 이름) 
- * @version (버전 번호나 날짜)
+ * @author (전 프로젝트- /이동-2018243087 정찬호) 
+ * @version (2019/09/09)
  */
 
 public interface Shape
 {
-    /**
-     *  예제 메소드 해더 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y    메소드의 예제 매개변수
-     * @return    예제메소드의 결과 산출물
-     */
-    int sampleMethod(int y);
+    final double PI = 3.14;
+    void draw();
+    double getArea();
+    default public void redraw(){
+        System.out.print("--- 다시 그립니다. ");
+        draw();
+    }
 }
